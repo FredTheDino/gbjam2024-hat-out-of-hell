94230b84f86e0302ecbb1dc310343039f5227e5a
Edvard Thörnros <edvard.thornros@gmail.com>
2024-09-15 22:49:31 +0200

    Some kind of type annotations?

--- gamestate.lua
+++ gamestate.lua
@@ -1,38 +1,56 @@
 local GameState = {}
 
--- interface GameState:
---  enter :: () -> state
---  exit :: (state) -> ()
---  update :: (state, inputs, dt) -> ()
---  draw :: (state) -> ()
-local active = nil
-local inputs = nil
+--- interface GameState:
+---  enter :: () -> state
+---  exit :: (state) -> ()
+---  update :: (state, inputs, dt) -> ()
+---  draw :: (state) -> ()
+---@class Instance
+local active = {
+  enter = function() return {} end,
+  exit = function(_) end,
+  update = function(_, _, _) end,
+  draw = function(_) end,
+  state = {}
+}
 
-function GameState.new(enter, exit, update, draw)
-  return setmetatable({
-    enter = enter,
-    exit = exit,
-    update = update,
-    draw = draw,
-    --
-    state = nil,
-  }, { __index = GameState })
+---@class Input
+local inputs = {
+  up = 0,
+  down = 0,
+  left = 0,
+  right = 0,
+  a = 0,
+  b = 0,
+  start = 0,
+  select = 0
+}
+
+--- @param state { enter: (fun(): table), exit: (fun(table)), update: (fun(table, inputs: Input, dt: number)), draw: (fun(table))  }
+--- @returns Instance
+function GameState.new(state)
+  return setmetatable(state
+  , { __index = GameState })
 end
 
+-- @param dt number
 function GameState.update(dt)
   if not active then return end
-  active:update(active.state, inputs, dt)
+  active.update(active.state, inputs, dt)
 end
 
 function GameState.draw()
   if not active then return end
-  active:draw(active.state)
+  active.draw(active.state)
 end
 
+--- @param new Instance
 function GameState.change(new)
   if active then
     active.exit(active.state)
   end
-  new.state = new.enter()
   active = new
+  active.state = active.enter()
 end
+
+return GameState
--- main.lua
+++ main.lua
@@ -1,15 +1,32 @@
-local player = require "player"
-local state = {
-  player = player.init()
+local Player = require "player"
+local _p = require "pprint"
+local GameState = require "gamestate"
+
+-- Example GameState
+local player_state = GameState.new {
+  enter = function()
+    return {
+      player = Player.init()
+    }
+  end,
+  exit = function() end,
+  update = function(x, inputs, dt)
+    x.player:update(dt)
+  end,
+  draw = function(state)
+    love.graphics.rectangle("fill", 1, 1, 100, 100)
+  end
 }
 
+
 function love.load()
+  GameState.change(player_state)
 end
 
 function love.update(dt)
-  state.player:update(dt)
+  GameState.update(dt)
 end
 
 function love.draw()
-  love.graphics.rectangle("fill", 1, 1, 100, 100)
+  GameState.draw()
 end
