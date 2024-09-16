4ff210288019a827dbac55f6c14bb4dfe4bbfd71
Gustav Sörnäs <gustav@sornas.net>
2024-09-16 15:48:42 +0200

    vector library, shooting

--- player.lua
+++ player.lua
@@ -1,4 +1,5 @@
 local joe = require "joe"
+local Vector = require "vector"
 
 ---@class Player
 local Player = {}
@@ -7,24 +8,39 @@ local Player = {}
 ---@return Player
 function Player.init()
   local self = setmetatable({}, { __index = Player })
-  self.x = 0.0
-  self.y = 0.0
+  self.pos = Vector(0.0, 0.0)
+  self.shoot_target = Vector(50, 50)
+  self.shoot_speed = 100
+  self.shoot1 = false
+  self.shoot2 = false
+  self.shoot1_cooldown = 0.0
+  self.shoot2_cooldown = 0.0
   return self
 end
 
-local speed = 10
+local speed = 40
 
 ---@param inputs Input
 ---@param dt number
 function Player:update(inputs, dt)
-  self.x = self.x + (joe.iff(inputs.right > 0, speed, 0) - joe.iff(inputs.left > 0, speed, 0)) * dt
-  self.y = self.y + (joe.iff(inputs.down > 0, speed, 0) - joe.iff(inputs.up > 0, speed, 0)) * dt
+  self.pos.x = self.pos.x + (joe.iff(inputs.right > 0, speed, 0) - joe.iff(inputs.left > 0, speed, 0)) * dt
+  self.pos.y = self.pos.y + (joe.iff(inputs.down > 0, speed, 0) - joe.iff(inputs.up > 0, speed, 0)) * dt
+
+  if inputs.a > 0.0 and self.shoot1_cooldown == 0.0 then
+    self.shoot1 = true
+    self.shoot1_cooldown = 0.2
+  else
+    self.shoot1 = false
+  end
+
+  self.shoot1_cooldown = math.max(0.0, self.shoot1_cooldown - dt)
+  self.shoot2_cooldown = math.max(0.0, self.shoot2_cooldown - dt)
 end
 
-local size = 10
+local size = 20
 
 function Player:draw()
-  love.graphics.rectangle("fill", self.x - size / 2, self.y - size / 2, size, size)
+  love.graphics.rectangle("fill", self.pos.x - size / 2, self.pos.y - size / 2, size, size)
 end
 
 return Player
