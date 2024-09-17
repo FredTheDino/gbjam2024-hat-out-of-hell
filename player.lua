948e5651bc63764dfa14fa470aec83781acf84d1
Edvard Thörnros <edvard.thornros@gmail.com>
2024-09-17 13:32:24 +0200

    More fluid movement

--- player.lua
+++ player.lua
@@ -1,5 +1,6 @@
 local joe = require "joe"
 local Vector = require "vector"
+local inspect = require "inspect"
 
 ---@class Player
 local Player = {}
@@ -9,6 +10,7 @@ local Player = {}
 function Player.init()
   local self = setmetatable({}, { __index = Player })
   self.pos = Vector(0.0, 0.0)
+  self.vel = Vector(0.0, 0.0)
   self.shoot_target = Vector(50, 50)
   self.shoot_speed = 100
   self.shot_life = 1.0
@@ -24,8 +26,14 @@ local speed = 40
 ---@param inputs Input
 ---@param dt number
 function Player:update(inputs, dt)
-  self.pos.x = self.pos.x + (joe.iff(inputs.right > 0, speed, 0) - joe.iff(inputs.left > 0, speed, 0)) * dt
-  self.pos.y = self.pos.y + (joe.iff(inputs.down > 0, speed, 0) - joe.iff(inputs.up > 0, speed, 0)) * dt
+  local movement_dir = (Vector.new(joe.iff(inputs.right > 0, speed, 0), 0)
+    + Vector.new(joe.iff(inputs.left > 0, -speed, 0), 0)
+    + Vector.new(0, joe.iff(inputs.down > 0, speed, 0))
+    + Vector.new(0, joe.iff(inputs.up > 0, -speed, 0))
+  )
+  self.vel = self.vel + movement_dir * dt
+  self.vel = self.vel * (0.1 ^ dt)
+  self.pos = self.pos + self.vel * dt
 
   if inputs.a > 0.0 and self.shoot1_cooldown == 0.0 then
     self.shoot1 = true
